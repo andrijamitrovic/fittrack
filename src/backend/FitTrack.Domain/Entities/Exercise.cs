030d37fa63a5435a58ce6dcc26194151246e9f8f
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-03-02 21:33:32 +0100

    feat: add entities for all database tables

---
+++ src/backend/FitTrack.Domain/Entities/Exercise.cs
@@ -0,0 +1,14 @@
+﻿namespace FitTrack.Domain.Entities
+{
+    public class Exercise
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public required string MuscleGroup { get; set; }
+        public string? Description { get; set; }
+        public required bool IsCustom { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public required DateTime CreatedAt { get; set; }
+    }
+}
