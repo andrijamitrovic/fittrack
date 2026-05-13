f98571f124f452db404510058d8ea31c0c545ec9
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-05-13 19:51:15 +0200

    feat: align e2e tests with other changes

--- src/backend/FitTrack.E2ETests/PageObjects/AddWorkoutPage.cs
+++ src/backend/FitTrack.E2ETests/PageObjects/AddWorkoutPage.cs
@@ -8,21 +8,21 @@ public class AddWorkoutPage
     public AddWorkoutPage(IPage page) => _page = page;
 
     public async Task GoToAsync() =>
-        await _page.GotoAsync(TestConfig.BaseUrl + "/workouts/new");
+        await _page.GotoAsync(TestConfig.BaseUrl + "/app/workouts/new");
 
     public async Task AddWorkoutAsync(string title, string notes, string selectOption, string reps, string weight, string rpe)
     {
         await GoToAsync();
-        await _page.GetByLabel("Title:").FillAsync(title);
-        await _page.GetByLabel("Notes:").FillAsync(notes);
+        await _page.GetByLabel("Title").FillAsync(title);
+        await _page.GetByLabel("Notes").FillAsync(notes);
 
-        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "+ Add exercise" }).ClickAsync(); 
-        await _page.GetByLabel("Exercise:").SelectOptionAsync(new SelectOptionValue { Label = selectOption });
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Add exercise" }).ClickAsync(); 
+        await _page.GetByLabel("Exercise").SelectOptionAsync(new SelectOptionValue { Label = selectOption });
         await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Add Set" }).ClickAsync();
 
-        await _page.GetByLabel("Reps:").FillAsync(reps);
-        await _page.GetByLabel("Weight:").FillAsync(weight);
-        await _page.GetByLabel("RPE:").FillAsync(rpe);
+        await _page.GetByLabel("Reps").FillAsync(reps);
+        await _page.GetByLabel("Weight").FillAsync(weight);
+        await _page.GetByLabel("RPE").FillAsync(rpe);
 
         await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Save Workout"}).ClickAsync();
     }
