5da2b1bd612f2c6d0c3f4a43ad997d2c9c2e66a7
Andrija Mitrovic <andrijamitrovic11@gmail.com>
2026-05-13 20:52:52 +0200

    bug: fix bugs

--- src/backend/FitTrack.E2ETests/PageObjects/AddWorkoutPage.cs
+++ src/backend/FitTrack.E2ETests/PageObjects/AddWorkoutPage.cs
@@ -10,20 +10,43 @@ public class AddWorkoutPage
     public async Task GoToAsync() =>
         await _page.GotoAsync(TestConfig.BaseUrl + "/app/workouts/new");
 
-    public async Task AddWorkoutAsync(string title, string notes, string selectOption, string reps, string weight, string rpe)
+    public async Task AddWorkoutAsync(
+        string title,
+        string notes,
+        string selectOption,
+        string reps,
+        string weight,
+        string rpe)
     {
         await GoToAsync();
+
         await _page.GetByLabel("Title").FillAsync(title);
         await _page.GetByLabel("Notes").FillAsync(notes);
 
-        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Add exercise" }).ClickAsync(); 
-        await _page.GetByLabel("Exercise").SelectOptionAsync(new SelectOptionValue { Label = selectOption });
-        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Add Set" }).ClickAsync();
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Add exercise"
+        }).ClickAsync();
 
-        await _page.GetByLabel("Reps").FillAsync(reps);
-        await _page.GetByLabel("Weight").FillAsync(weight);
-        await _page.GetByLabel("RPE").FillAsync(rpe);
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Pick exercise"
+        }).ClickAsync();
 
-        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Save Workout"}).ClickAsync();
+        await _page.GetByText(selectOption).ClickAsync();
+
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Add set"
+        }).ClickAsync();
+
+        await _page.GetByRole(AriaRole.Spinbutton).Nth(0).FillAsync(reps);
+        await _page.GetByRole(AriaRole.Spinbutton).Nth(1).FillAsync(weight);
+        await _page.GetByRole(AriaRole.Spinbutton).Nth(2).FillAsync(rpe);
+
+        await _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions
+        {
+            Name = "Save workout"
+        }).ClickAsync();
     }
 }
